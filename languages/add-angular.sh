71145e9f63aa6804483c2666d8590b110578a1f4
Chris Bostwick <chronic.crash@gmail.com>
2022-07-09 16:11:13 -0700

    added nvm

--- languages/add-angular.sh
+++ languages/add-angular.sh
@@ -1,4 +1,7 @@
+sudo apt install curl
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
 sudo apt-get install -y nodejs
 sudo apt install -y npm
 sudo npm install npm@latest -g
